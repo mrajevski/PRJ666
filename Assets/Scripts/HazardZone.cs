4709b878e90dca9cde114032fdd2447761baaa46
Kinshu <kc007919@gmail.com>
2018-04-07 20:31:56 -0400

    Added Deaths Screen & Posion Gas

--- Assets/Scripts/HazardZone.cs
+++ Assets/Scripts/HazardZone.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class HazardZone : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public GameObject player;
+    public double damageDealt;
+    public BarScript healthbar;
+
+    private float counter;
+
+    // Use this for initialization
+    void Start () {
+        counter = 0;
+        damageDealt = 0;
+        player = GameObject.FindGameObjectWithTag("Player");
+        healthbar = FindObjectOfType<BarScript>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        counter++;
+        damageDealt = Time.deltaTime + 0.1;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.name == "Player")
+        {
+            healthbar.posionState = true;
+            //if ()//TODO: Add when implement the inv
+            if (counter % 2 == 0)
+            {
+                player.GetComponent<playerHealth>().posionTick((float)damageDealt);
+            }
+        }
+        else
+        {
+            healthbar.posionState = false;
+        }
+    }
+
 }
---
+++ Assets/Scripts/deadMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class deadMenu : MonoBehaviour {
+
+    public Button menu;
+
+	// Use this for initialization
+	void Start () {
+		menu = menu.GetComponent<Button>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void toMenuPressed()
+    {
+        SceneManager.LoadScene("menu");
+    }
+}
