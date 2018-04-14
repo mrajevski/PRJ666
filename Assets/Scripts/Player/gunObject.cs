9eb6f95645ee079f4fdb67e4e7f63b33a44fce80
Kelvin Cho <25650647+TheKinshu@users.noreply.github.com>
2018-04-13 20:42:56 -0400

    Revert "Shop"

--- Assets/Scripts/Player/gunObject.cs
+++ Assets/Scripts/Player/gunObject.cs
@@ -5,23 +5,8 @@ using UnityEngine;
 public class gunObject : MonoBehaviour {
 
 	public float reloadTime, accuracy, rateOfFire;
-	public int damage, magCapacity, gunID, ammoID, value;
-	public short fireMode /* 0 - Full Auto, 1 - Single Shot, 2 - Shotgun*/;
-	public AudioSource gunAudio, reloadAudio;
+	public int damage, magCapacity, gunID, ammoID;
+	public AudioSource gunAudio, gunAudioSingle, reloadAudio;
 
-
-	public gunObject() {
-		reloadTime = -1.0f;
-		accuracy = -1.0f;
-		rateOfFire = -1.0f;
-		damage = -1;
-		magCapacity = -1;
-		gunID = -1;
-		ammoID = -1;
-		value = -1;
-		fireMode = -1;
-		gunAudio = null;
-		reloadAudio = null;
-	}
 
 }
